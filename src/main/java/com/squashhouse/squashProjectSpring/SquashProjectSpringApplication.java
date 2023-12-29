31128bcc82aeb8fbdc316c22badda05d9d636fa5
PiotrKupis999 <piotr.kupis999@gmail.com>
2023-12-29 19:33:19 +0100

    projectConfiguration
    spring config

---
+++ src/main/java/com/squashhouse/squashProjectSpring/SquashProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.squashhouse.squashProjectSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquashProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquashProjectSpringApplication.class, args);
+	}
+
+}
